eaaa4a5160d8364a370f988940575beca151e00d
doshanlal-coder <doshanlals@gmail.com>
2026-08-29 20:11:29 +0530

    Started Solving Striver's A2Z sheet

---
+++ Easy.cpp
@@ -0,0 +1,18 @@
+//Given an array arr of n elements. The task is to reverse the given array. The reversal of array should be inplaca
+
+
+// class Solution{
+// public:
+//     void reverse(vector<int>& arr){
+//         int n = arr.size();
+//         int left = 0;
+//         int right = n-1;
+//         while(left<=right){
+//             swap(arr[left],arr[right]);
+//             left++;
+//             right--;
+//         }
+        
+//     }
+// };
+
